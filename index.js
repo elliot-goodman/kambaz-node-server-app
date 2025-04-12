6897b96c9d1faabf5a533e4a9dc17da812d3c203
Elliot Goodman <elliotgoodman@elliots-air.mynetworksettings.com>
2025-04-12 15:41:28 -0400

    finished 6.2.6.7

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import mongoose from "mongoose";
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
 import UserRoutes from "./Kambaz/Users/routes.js";
@@ -9,6 +10,9 @@ import "dotenv/config";
 import AssignmentRoutes from "./Kambaz/Assignments/routes.js";
 import EnrollmentRoutes from "./Kambaz/Enrollments/routes.js";
 
+const CONNECTION_STRING =
+  process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz";
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 
 app.use(
