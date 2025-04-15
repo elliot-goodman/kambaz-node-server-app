a2b8948a49d8144b2a49460802268b9db6ee2078
Elliot Goodman <elliotgoodman@Elliots-MacBook-Air.local>
2025-04-15 16:29:48 -0400

    added database features

--- Kambaz/Enrollments/dao.js
+++ Kambaz/Enrollments/dao.js
@@ -1,48 +1,16 @@
-import Database from "../Database/index.js";
-
-export function enrollUserInCourse(userId, courseId) {
-  const { enrollments } = Database;
-  const enrollment = {
-    _id: Date.now().toString(),
-    user: userId,
-    course: courseId,
-  };
-  enrollments.push(enrollment);
-  return enrollment;
+import model from "./model.js";
+export async function findCoursesForUser(userId) {
+  const enrollments = await model.find({ user: userId }).populate("course");
+  return enrollments.map((enrollment) => enrollment.course);
 }
-
-export function getEnrollmentsForUser(userId) {
-  const { enrollments } = Database;
-  return enrollments.filter((enrollment) => enrollment.user === userId);
+export async function findUsersForCourse(courseId) {
+  const enrollments = await model.find({ course: courseId }).populate("user");
+  return enrollments.map((enrollment) => enrollment.user);
 }
-
-export function deleteEnrollment(enrollmentId) {
-  const { enrollments } = Database;
-  Database.enrollments = enrollments.filter(
-    (enrollment) => enrollment._id !== enrollmentId
-  );
+export function enrollUserInCourse(user, course) {
+  const newEnrollment = { user, course, _id: `${user}-${course}` };
+  return model.create(newEnrollment);
 }
-
-// import Database from "../Database/index.js";
-// import { v4 as uuidv4 } from "uuid";
-
-// export function findEnrollmentsForCourse(courseId) {
-//   const { enrollments } = Database;
-//   return enrollments.filter((enrollment) => enrollment.course === courseId);
-// }
-
-// export function createEnrollment(enrollment) {
-//   const newEnrollment = { ...enrollment, _id: uuidv4() };
-//   Database.enrollments = [...Database.enrollments, newEnrollment];
-//   return newEnrollment;
-// }
-
-// export function enrollUserInCourse(id, userId, courseId) {
-//   const { enrollments } = Database;
-//   enrollments.push({ _id: id, user: userId, course: courseId });
-// }
-
-// export function findCoursesForUser(userId) {
-//   const { enrollments } = Database;
-//   return enrollments.filter((enrollment) => enrollment.user === userId);
-// }
+export function unenrollUserFromCourse(user, course) {
+  return model.deleteOne({ user, course });
+}
--- Kambaz/Enrollments/routes.js
+++ Kambaz/Enrollments/routes.js
@@ -3,8 +3,7 @@ import * as enrollmentsDao from "./dao.js";
 export default function EnrollmentRoutes(app) {
   app.get("/api/enrollments/:userId", async (req, res) => {
     const { userId } = req.params;
-    console.log("getting enrollments for user", userId);
-    const enrollments = await enrollmentsDao.getEnrollmentsForUser(userId);
+    const enrollments = await enrollmentsDao.findCoursesForUser(userId);
     res.send(enrollments);
   });
 
@@ -20,18 +19,3 @@ export default function EnrollmentRoutes(app) {
     res.send(status);
   });
 }
-
-// import * as enrollmentsDao from "./dao.js";
-
-// export default function EnrollmentRoutes(app) {
-//   app.get("/api/enrollments/:userId", async (req, res) => {
-//     const { userId } = req.params;
-//     const enrollments = await enrollmentsDao.findCoursesForUser(userId);
-//     res.send(enrollments);
-//   });
-//   app.post("/api/enrollments", async (req, res) => {
-//     const { user, course } = req.body;
-//     const newEnrollment = await enrollmentsDao.enrollUserInCourse(user, course);
-//     res.send(newEnrollment);
-//   });
-// }
